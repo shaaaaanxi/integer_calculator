81524c90a248a34b1896cd8163a51f5435988a14
shaaaaanxi <39790632+shaaaaanxi@users.noreply.github.com>
2018-05-31 18:47:19 +0900

    Update main.c

--- Project1/Project1/main.c
+++ Project1/Project1/main.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 
+void multi(int *num[100], char *cal[100]) {
+int result;
+int max = strlen(num);
+
+for (int i = 0;i<max; i++) {
+	if (cal[i] == '42') {
+		result = num[i] * num[i + 1];
+		num[i] = result;
+		for (int j = i; j < max-1; j++) {
+			cal[j] = cal[j + 1];
+			num[j + 1] = num[j + 2];
+		}
+	}
+}
+}
+
 int main() {
 
 	char cal[10];
